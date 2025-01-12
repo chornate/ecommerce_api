4afa0d1df6b26e5838f1352564020ed434689de1
chornate <155074527+chornate@users.noreply.github.com>
2025-01-12 12:08:44 +0300

    fixed login and registration

--- api/views.py
+++ api/views.py
@@ -63,25 +63,25 @@ class OrderItemViewSet(ModelViewSet):
 def register_user(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        username = data.get('username')
+        email = data.get('email')
         password = data.get('password')
-        if username and password:
+        if email and password:
             try:
-                user = User.objects.create_user(username=username, password=password)
+                user = User.objects.create_user(email=email, password=password)
                 return JsonResponse({'message': 'User registered successfully'})
             except Exception as e:
                 return JsonResponse({'error': str(e)}, status=400)
         else:
-            return JsonResponse({'error': 'Username and password are required'}, status=400)
+            return JsonResponse({'error': 'Email and password are required'}, status=400)
     return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 @csrf_exempt
 def login_user(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        username = data.get('username')
+        email = data.get('email')
         password = data.get('password')
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
             return JsonResponse({'message': 'User logged in successfully'})
